9d63dc57553fdd1fc67f8c535f915af879b18fbf
Ellyboi <vitoesitemitope@gmail.com>
2023-08-09 23:10:04 +0100

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :groups
-  has_many :entities, foreign_key: :author_id
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 } # Adjust minimum length as needed
+        
+  has_many :groups, dependent: :destroy
+  has_many :entities, foreign_key: :author_id,dependent: :destroy
 end
