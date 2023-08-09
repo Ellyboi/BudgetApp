78e51cf453a8076b77a682261a1046edc7a977eb
Ellyboi <vitoesitemitope@gmail.com>
2023-08-09 21:24:38 +0100

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  
+  root to: "groups#index"
   resources :users do
     resources :entities
     resources :groups
+  end
+
+  resources :groups do
+    resources :entities
+  end
+
+  resources :entities
 end
