5d0b9fba4913e4d205aaac5b4aece5f9c3530600
Ellyboi <vitoesitemitope@gmail.com>
2023-08-11 16:34:24 +0100

    update styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_160805) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_200856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_160805) do
     t.datetime "createdAt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -46,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_160805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
   add_foreign_key "groups", "users"
 end
